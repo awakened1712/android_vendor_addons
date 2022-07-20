b8f5b997e28f089f28171016e86ef340ae2b6d59
minaripenguin <alexfinhart@gmail.com>
2022-07-20 21:21:51 +0800

    addons: Update Icon shapes for System shapes feature
    * update icon shapes overlays
    * introduce google, defaultbutsquare and rice shape overlays
    * remove legacy overrides/overlays

--- config.mk
+++ config.mk
@@ -211,13 +211,20 @@ PRODUCT_PACKAGES += \
 # Icon Shapes
 PRODUCT_PACKAGES += \
     IconShapeCloudyOverlay \
+    IconShapeCloudyRoundedOverlay \
     IconShapeCylinderOverlay \
+    IconShapeCylinderRoundedOverlay \
     IconShapeFlowerOverlay \
+    IconShapeFlowerRoundedOverlay \
     IconShapeHeartOverlay \
+    IconShapeHeartRoundedOverlay \
     IconShapeHexagonOverlay \
+    IconShapeLeafRoundedOverlay \
+    IconShapePebbleRoundedOverlay \
     IconShapeLeafOverlay \
     IconShapeMeowOverlay \
     IconShapePebbleOverlay \
+    IconShapeRiceBallsOverlay \
     IconShapeRoundedHexagonOverlay \
     IconShapeRoundedRectOverlay \
     IconShapeSquareOverlay \
@@ -225,7 +232,10 @@ PRODUCT_PACKAGES += \
     IconShapeStretchedOverlay \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
-    IconShapeVesselOverlay
+    IconShapeVesselOverlay \
+    DefaultButSquareOverlay \
+    DefaultButBouncyOverlay \
+    GoogleShapesOverlay
 
 # Navbar
 PRODUCT_PACKAGES += \
